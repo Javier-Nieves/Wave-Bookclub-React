4bd2dfbd33c4cc9db7d9bb5ed498f465f9efdcf2
Javier Nieves <javier.nikolskiy@gmail.com>
2026-07-13 19:17:01 +0200

    API countries 1

--- src/services/apiCountries.js
+++ src/services/apiCountries.js
@@ -10,7 +10,7 @@ export async function getCountries() {
   // testing:
   //   await new Promise((res) => setTimeout(res, 5000));
 
-  const response = await fetch(COUNTRIES_API);
+  const response = await fetch(COUNTRIES_API, { headers: { 'Authorization': 'Bearer rc_live_db6c92463d89411499a12745ad770d9a' } });
   const data = await response.json();
 
   const updatedData = data.map((item) => {
