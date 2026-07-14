31db2f872ba7cb1ff194f86bd1f7b23f834c6c5f
Javier Nieves <javier.nikolskiy@gmail.com>
2026-07-14 14:00:05 +0200

    countries API fix 7

--- src/services/apiCountries.js
+++ src/services/apiCountries.js
@@ -1,4 +1,4 @@
-import { COUNTRIES_API } from "../utils/config";
+import { COUNTRIES_API, COUNTRIES_API_TOKEN } from "../utils/config";
 
 export async function getCountries() {
   // racing countries fetch request with TIMEOUT_SEC clock:
@@ -11,19 +11,19 @@ export async function getCountries() {
   //   await new Promise((res) => setTimeout(res, 5000));
 
   // const response = await fetch(COUNTRIES_API, 
-  //   { headers: { 'Authorization': 'Bearer rc_live_db6c92463d89411499a12745ad770d9a' } });
+  //   { headers: { 'Authorization': COUNTRIES_API_TOKEN } });
   // const data = await response.json();
-      const response1 = await fetch(`${COUNTRIES_API}`,
-      { headers: { 'Authorization': 'Bearer rc_live_db6c92463d89411499a12745ad770d9a' } }
+      const response1 = await fetch(COUNTRIES_API,
+      { headers: { 'Authorization': COUNTRIES_API_TOKEN } }
     );
     const data = await response1.json();
     const response2 = await fetch(`${COUNTRIES_API}&offset=100`,
-      { headers: { 'Authorization': 'Bearer rc_live_db6c92463d89411499a12745ad770d9a' } }
+      { headers: { 'Authorization': COUNTRIES_API_TOKEN } }
     );
     const data2 = await response2.json();
     const response3 = await fetch(
       `${COUNTRIES_API}&offset=200`,
-      { headers: { 'Authorization': 'Bearer rc_live_db6c92463d89411499a12745ad770d9a' } }
+      { headers: { 'Authorization': COUNTRIES_API_TOKEN } }
     );
     const data3 = await response3.json();
     data.data.objects = [...data.data.objects,...data2.data.objects,...data3.data.objects]
